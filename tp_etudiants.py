9b85c03fa992ac0d931668ca22b0bb862b10d950
ouassimcbkl <129180627+ouassimcbkl@users.noreply.github.com>
2026-03-25 13:27:58 +0100

    Refactor student iterators for different subjects

--- tp_etudiants.py
+++ tp_etudiants.py
@@ -8,10 +8,38 @@ class Student:
         self.info_grade = info_grade
 
 
-class StudentIterator(Iterator):
+# Itérateur matière 1 (math)
+class MathIterator(Iterator):
     def __init__(self, students):
-        # trier décroissant sur la matière 1 (math_grade)
         self.sorted_students = sorted(students, key=lambda s: s.math_grade, reverse=True)
+        self.index = 0
+
+    def __next__(self):
+        if self.index < len(self.sorted_students):
+            student = self.sorted_students[self.index]
+            self.index += 1
+            return student
+        else:
+            raise StopIteration
+
+# Itérateur matière 2 (physique)
+class PhysicsIterator(Iterator):
+    def __init__(self, students):
+        self.sorted_students = sorted(students, key=lambda s: s.physics_grade, reverse=True)
+        self.index = 0
+
+    def __next__(self):
+        if self.index < len(self.sorted_students):
+            student = self.sorted_students[self.index]
+            self.index += 1
+            return student
+        else:
+            raise StopIteration
+
+# Itérateur matière 3 (info)
+class InfoIterator(Iterator):
+    def __init__(self, students):
+        self.sorted_students = sorted(students, key=lambda s: s.info_grade, reverse=True)
         self.index = 0
 
     def __next__(self):
@@ -30,8 +58,15 @@ class SchoolClass(Iterable):
     def add_student(self, student):
         self.students.append(student)
 
+    # itérable par défaut → matière 1
     def __iter__(self):
-        return StudentIterator(self.students)
+        return MathIterator(self.students)
+
+    def iter_matter_2(self):
+        return PhysicsIterator(self.students)
+
+    def iter_matter_3(self):
+        return InfoIterator(self.students)
 
 
 # --- Bloc main ---
@@ -40,7 +75,14 @@ school_class.add_student(Student('J', 10, 12, 13))
 school_class.add_student(Student('A', 8, 2, 17))
 school_class.add_student(Student('V', 9, 14, 14))
 
-# Utilisation de l'itérateur
-print("Classement matière 1 via itérateur :")
+print("Classement matière 1 (math) :")
 for student in school_class:
     print(student.name, ":", student.math_grade)
+
+print("\nClassement matière 2 (physique) :")
+for student in school_class.iter_matter_2():
+    print(student.name, ":", student.physics_grade)
+
+print("\nClassement matière 3 (info) :")
+for student in school_class.iter_matter_3():
+    print(student.name, ":", student.info_grade)
