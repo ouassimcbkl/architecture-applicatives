158db4c04657417d56b79c697d6101def5349c63
ouassimcbkl <129180627+ouassimcbkl@users.noreply.github.com>
2026-03-25 13:22:29 +0100

    Implement StudentIterator for ranking students
    choolClass(Iterable) :
    Hérite d’Iterable pour dire que l’on peut faire for student in school_class.
    StudentIterator(Iterator) :
    Tri les étudiants sur math_grade dans __init__.
    __next__() renvoie le prochain étudiant jusqu’à ce qu’on atteigne la fin.
    Bloc main :
    for student in school_class: utilise automatiquement __iter__().
    L’ordre est déjà du meilleur au plus mauvais en matière 1.

--- tp_etudiants.py
+++ tp_etudiants.py
@@ -1,3 +1,5 @@
+from collections.abc import Iterable, Iterator
+
 class Student:
     def __init__(self, name, math_grade, physics_grade, info_grade):
         self.name = name
@@ -6,47 +8,30 @@ class Student:
         self.info_grade = info_grade
 
 
-class SchoolClass:
+class StudentIterator(Iterator):
+    def __init__(self, students):
+        # trier décroissant sur la matière 1 (math_grade)
+        self.sorted_students = sorted(students, key=lambda s: s.math_grade, reverse=True)
+        self.index = 0
+
+    def __next__(self):
+        if self.index < len(self.sorted_students):
+            student = self.sorted_students[self.index]
+            self.index += 1
+            return student
+        else:
+            raise StopIteration
+
+
+class SchoolClass(Iterable):
     def __init__(self):
         self.students = []
 
     def add_student(self, student):
         self.students.append(student)
 
-    def rank_matter_1(self):
-        sorted_students = sorted(
-            self.students,
-            key=lambda student: student.math_grade,
-            reverse=True
-        )
-
-        print("Classement matière 1 (maths) :")
-        for student in sorted_students:
-            print(student.name, ":", student.math_grade)
-
-    # Nouvelle méthode demandée
-    def rank_matter_2(self):
-        sorted_students = sorted(
-            self.students,
-            key=lambda student: student.physics_grade,
-            reverse=True
-        )
-
-        print("Classement matière 2 (physique) :")
-        for student in sorted_students:
-            print(student.name, ":", student.physics_grade)
-
-    # Nouvelle méthode demandée
-    def rank_matter_3(self):
-        sorted_students = sorted(
-            self.students,
-            key=lambda student: student.info_grade,
-            reverse=True
-        )
-
-        print("Classement matière 3 (informatique) :")
-        for student in sorted_students:
-            print(student.name, ":", student.info_grade)
+    def __iter__(self):
+        return StudentIterator(self.students)
 
 
 # --- Bloc main ---
@@ -55,6 +40,7 @@ school_class.add_student(Student('J', 10, 12, 13))
 school_class.add_student(Student('A', 8, 2, 17))
 school_class.add_student(Student('V', 9, 14, 14))
 
-school_class.rank_matter_1()
-school_class.rank_matter_2()
-school_class.rank_matter_3()
+# Utilisation de l'itérateur
+print("Classement matière 1 via itérateur :")
+for student in school_class:
+    print(student.name, ":", student.math_grade)
