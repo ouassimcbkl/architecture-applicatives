dee1d16c7f3d93263282764d159bc8e985ae8024
ouassimcbkl <129180627+ouassimcbkl@users.noreply.github.com>
2026-03-25 13:35:25 +0100

    Refactor decorators and implement singleton pattern

--- tp_etudiants.py
+++ tp_etudiants.py
@@ -1,16 +1,21 @@
 from collections.abc import Iterable, Iterator
 
-# --- Décorateur pour Student (matière 4) ---
+# --- Décorateurs pour Student et SchoolClass ---
 def add_chemistry(cls):
     original_init = cls.__init__
-
     def new_init(self, *args, **kwargs):
         original_init(self, *args, **kwargs)
-        self.chem_grade = 0  # note par défaut pour chimie
-
+        self.chem_grade = 0
     cls.__init__ = new_init
     return cls
 
+def add_matter_4_iterator(cls):
+    def iter_matter_4(self):
+        return ChemIterator(self.students)
+    cls.iter_matter_4 = iter_matter_4
+    return cls
+
+# --- Classe Student ---
 @add_chemistry
 class Student:
     def __init__(self, name, math_grade, physics_grade, info_grade):
@@ -18,7 +23,6 @@ class Student:
         self.math_grade = math_grade
         self.physics_grade = physics_grade
         self.info_grade = info_grade
-
 
 # --- Itérateurs ---
 class MathIterator(Iterator):
@@ -69,17 +73,18 @@ class ChemIterator(Iterator):
         else:
             raise StopIteration
 
-# --- Décorateur pour ajouter la méthode iter_matter_4 à SchoolClass ---
-def add_matter_4_iterator(cls):
-    def iter_matter_4(self):
-        return ChemIterator(self.students)
-    cls.iter_matter_4 = iter_matter_4
-    return cls
-
+# --- Singleton + décorateur pour itérateur matière 4 ---
 @add_matter_4_iterator
 class SchoolClass(Iterable):
+    _instance = None
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
     def __init__(self):
-        self.students = []
+        if not hasattr(self, 'students'):
+            self.students = []
 
     def add_student(self, student):
         self.students.append(student)
@@ -95,16 +100,20 @@ class SchoolClass(Iterable):
 
 
 # --- Bloc main ---
-school_class = SchoolClass()
-school_class.add_student(Student('J', 10, 12, 13))
-school_class.add_student(Student('A', 8, 2, 17))
-school_class.add_student(Student('V', 9, 14, 14))
+# Peu importe où on crée l'instance, c'est la même
+school_class1 = SchoolClass()
+school_class2 = SchoolClass()
 
-# Mise à jour des notes de chimie
-school_class.students[0].chem_grade = 15
-school_class.students[1].chem_grade = 12
-school_class.students[2].chem_grade = 18
+school_class1.add_student(Student('J', 10, 12, 13))
+school_class2.add_student(Student('A', 8, 2, 17))
+school_class1.add_student(Student('V', 9, 14, 14))
 
-print("Classement matière 4 (chimie) via décorateur :")
-for s in school_class.iter_matter_4():
+# Mise à jour chimie
+school_class1.students[0].chem_grade = 15
+school_class1.students[1].chem_grade = 12
+school_class1.students[2].chem_grade = 18
+
+# Vérification singleton : school_class1 et school_class2 contiennent les mêmes étudiants
+print("Classement matière 4 (chimie) via singleton :")
+for s in school_class2.iter_matter_4():
     print(s.name, ":", s.chem_grade)
