ecabef0bd4e4fcb6eb41386ddcd29a852afe6aa9
ouassimcbkl <129180627+ouassimcbkl@users.noreply.github.com>
2026-03-25 13:04:06 +0100

    Rename classes and update method names for clarity

--- tp_etudiants.py
+++ tp_etudiants.py
@@ -1,14 +1,21 @@
-class Etudiant:
-    def __init__(self, nom, note_maths, note_physique, note_info):
-        self.nom = nom
-        self.note_maths = note_maths
-        self.note_physique = note_physique
-        self.note_info = note_info
+class Student:
+    def __init__(self, name, math_grade, physics_grade, info_grade):
+        self.name = name
+        self.math_grade = math_grade
+        self.physics_grade = physics_grade
+        self.info_grade = info_grade
 
 
-class Classe:
+class SchoolClass:
     def __init__(self):
-        self.etudiants = []
+        self.students = []
 
-    def ajouter_etudiant(self, etudiant):
-        self.etudiants.append(etudiant)
+    def add_student(self, student):
+        self.students.append(student)
+
+
+# --- Code de test demandé par le professeur ---
+school_class = SchoolClass()
+school_class.add_student(Student('J', 10, 12, 13))
+school_class.add_student(Student('A', 8, 2, 17))
+school_class.add_student(Student('V', 9, 14, 14))
